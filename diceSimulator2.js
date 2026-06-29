0f2c32dea88030f93c1a1a20fa6d922bee9f7ca4
Sudaice <charmingsudaice@gmail.com>
2026-06-29 22:22:35 +0300

    Add files via upload

---
+++ diceSimulator2.js
@@ -0,0 +1,15 @@
+function roll() {
+    let numberOfDice = document.getElementById("numberOfDice").value;
+    let results = document.getElementById("results");
+    let values = [];
+    let images = [];
+    for(let i = 0; i < numberOfDice; i++) {
+        let value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+    }
+    for(let i = 0; i < values.length; i++) {
+        let image = `<figure><figcaption class="value">${values[i]}</figcaption><img src="diceImages/dice${values[i]}.png" alt="dice${values[i]}" class="dice"></figure>`;
+        images.push(image);
+    }
+    results.innerHTML = images.join("");
+}
