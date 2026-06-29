bd391768dcf9fc6071ad2fac375a5d92cfd8dbc2
Sudaice <charmingsudaice@gmail.com>
2026-06-29 22:18:12 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-let roll = document.getElementById("roll-button");
-let dice = document.getElementById("dice");
-roll.onclick = function() {
-    diceValue = (Math.floor(Math.random() * 6) + 1);
-    dice.textContent = diceValue;
-}
